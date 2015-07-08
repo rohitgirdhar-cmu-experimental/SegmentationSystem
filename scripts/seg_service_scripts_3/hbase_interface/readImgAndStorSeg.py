badc28c7c82a16e22eeb748ecf6c7f7274f98a1c
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-07-08 13:45:14 +0000

    script to read and store all segmentations

---
+++ scripts/seg_service_scripts_3/hbase_interface/readImgAndStorSeg.py
@@ -0,0 +1,67 @@
+import happybase as hb
+import zmq
+import base64
+import time
+import sys
+import os
+
+#### CONFIG
+hbase_url = '10.1.94.57'
+hbase_table = 'roxyscrape'
+temp_img_path = './temp-dir/img.jpg'
+seg_service_port = 5561
+output_hbase_table = 'rgirdhar-CMU-segmentation'
+seg_fpath = '../temp-dir/result.jpg'
+loc_fpath = '../temp-dir/locResult.txt'
+###########
+
+last_print_time = -1
+def tic_toc_print(str):
+  global last_print_time
+  if time.time() - last_print_time > 1:
+    sys.stdout.write(str)
+    last_print_time = time.time()
+
+def refineBox(bbox):
+  OFFSET = (256 -227) / 2
+  return [str(float(el) + OFFSET) for el in bbox]
+
+
+# set up hbase
+conn = hb.Connection(hbase_url)
+tab = conn.table(hbase_table)
+out_tab = conn.table(output_hbase_table)
+# set up zmq
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect('tcp://localhost:%d' % seg_service_port)
+
+total_time_elap = 0
+nitem = 0
+for key,data in tab.scan():
+  start_time = time.time()
+  # get image
+  raw_img = base64.b64decode(data['image:orig'])
+  f = open(temp_img_path, 'wb')
+  f.write(raw_img)
+  f.close()
+  # run segmentation
+  socket.send(os.path.realpath(temp_img_path) + '\0')
+  socket.recv()
+  f = open(seg_fpath)
+  seg_output = base64.b64encode(f.read())
+  f.close()
+  f = open(loc_fpath)
+  loc_output = refineBox(f.read().strip().split(' ')[1:])
+  f.close()
+  #writing it out
+  out_tab.put(key, {'seg:orig' : seg_output, 
+      'loc:xmin' : loc_output[0],
+      'loc:xmax' : loc_output[1],
+      'loc:ymin' : loc_output[2],
+      'loc:ymax' : loc_output[3]})
+
+  end_time = time.time()
+  time_elap = end_time - start_time
+  tic_toc_print('Elapsed %f sec\n' % time_elap)
+
